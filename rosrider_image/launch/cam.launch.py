b4965a0d2e7f29a97da9e45435bdbbf51a409c18
Can Altineller <can@acada.dev>
2026-06-10 21:05:24 +0300

    tune9

--- rosrider_image/launch/cam.launch.py
+++ rosrider_image/launch/cam.launch.py
@@ -5,6 +5,11 @@ from launch_ros.actions import Node
 
 config = os.path.join(get_package_share_directory('rosrider_image'), 'param', 'cam.yaml')
 
+# add flag for rect
+# TODO: insructions for calibration
+# TODO: how to place the calibration file
+# TODO: lower fps
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
@@ -14,6 +19,7 @@ def generate_launch_description():
             name='v4l2_camera_node',
             parameters=[config]
         ),
+        '''
         Node(
             package='image_proc',
             executable='rectify_node',
@@ -23,6 +29,7 @@ def generate_launch_description():
                 ('camera_info', '/camera/camera_info'),
             ]
         )
+        '''
     ])
 
 
