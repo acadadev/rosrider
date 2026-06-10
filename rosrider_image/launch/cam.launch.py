1d170f259b61366b4f7f13893dda332c1125a776
Can Altineller <can@acada.dev>
2026-06-10 21:37:56 +0300

    tune12

--- rosrider_image/launch/cam.launch.py
+++ rosrider_image/launch/cam.launch.py
@@ -1,6 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 config = os.path.join(get_package_share_directory('rosrider_image'), 'param', 'cam.yaml')
